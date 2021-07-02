7b1696e31bca4ccb8853024120257a270ad536af
Jason Smith <SmiJa@users.noreply.github.com>
2021-07-01 23:18:33 -0700

    Setting up more variables

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
 const playerRock = document.getElementById("item-1");
 const playerPaper = document.getElementById("item-2");
 const playerScissors = document.getElementById("item-3");
+const computerSelection = document.getElementById("compueter-selection");
+const outcome = document.getElementById("outcome");
