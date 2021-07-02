efc5218a628a80e4dc05d31f758aec53b548d53a
Jason Smith <SmiJa@users.noreply.github.com>
2021-07-01 23:27:26 -0700

    Fixed spelling error in computerSelection variable

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const playerRock = document.getElementById("item-1");
 const playerPaper = document.getElementById("item-2");
 const playerScissors = document.getElementById("item-3");
-const computerSelection = document.getElementById("compueter-selection");
+const computerSelection = document.getElementById("computer-selection");
 const outcome = document.getElementById("outcome");
