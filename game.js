8b674c32267a9219de67916467150f809cdc8bf2
Jason Smith <SmiJa@users.noreply.github.com>
2021-07-14 22:29:56 -0700

    Added two functions

--- game.js
+++ game.js
@@ -3,3 +3,13 @@ const playerPaper = document.getElementById("item-2");
 const playerScissors = document.getElementById("item-3");
 const computerSelection = document.getElementById("computer-selection");
 const outcome = document.getElementById("outcome");
+
+function compChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let num = randNum();
+    return choices[num];
+}
+
+function randNum() {
+    return Math.random(2 - 0) + 0;
+}
